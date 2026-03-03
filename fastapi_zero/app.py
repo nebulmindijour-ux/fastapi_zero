374c46c297dd321eb188feb96cab98c1f6f8afdb
nebulmindijour-ux <nebulmindijour@gmail.com>
2026-03-03 17:49:59 -0300

    Adicionando Schemas de messagem e teste de HTML

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,26 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from fastapi_zero.schemas import Message
+
+app = FastAPI(title='Teste de Api!')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'Message': 'Ola Mundo!'}
+    return {'message': 'Ola Mundo!'}
+
+
+@app.get('/HTML', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def HTML():
+    return """
+    <html>
+        <head>
+            <title> Nosso ola Mundo!</title>
+        </head>
+        <body>
+            <h1> Ola Mundo </h1>
+        </body>
+    </html>"""
